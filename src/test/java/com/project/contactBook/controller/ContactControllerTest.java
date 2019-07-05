160c054ece05fdc37c4961edbe62ba65aa4d2f50
Donvl <valerijdoktor@gmail.com>
2019-07-05 13:14:02 +0300

    Test

---
+++ src/test/java/com/project/contactBook/controller/ContactControllerTest.java
@@ -0,0 +1,23 @@
+package com.project.contactBook.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 111 on 05.07.2019.
+ */
+public class ContactControllerTest {
+    @Test
+    public void getContactsList() throws Exception {
+    }
+
+    @Test
+    public void createContact() throws Exception {
+    }
+
+    @Test
+    public void updateContact() throws Exception {
+    }
+
+}
---
+++ src/test/java/com/project/contactBook/controller/PhoneNumberControllerTest.java
@@ -0,0 +1,19 @@
+package com.project.contactBook.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 111 on 05.07.2019.
+ */
+public class PhoneNumberControllerTest {
+    @Test
+    public void addPhoneNumber() throws Exception {
+    }
+
+    @Test
+    public void updatePhoneNumber() throws Exception {
+    }
+
+}
