31f2ec028f5a69a2405bee97f1e6ea2a178ff98c
Donvl <valerijdoktor@gmail.com>
2019-07-05 13:10:27 +0300

    Test

---
+++ src/test/java/com/project/contactBook/util/ContactBookUtilTest.java
@@ -0,0 +1,31 @@
+package com.project.contactBook.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 111 on 05.07.2019.
+ */
+public class ContactBookUtilTest {
+    @Test
+    public void getContactsListWithPhoneNumbers() throws Exception {
+    }
+
+    @Test
+    public void createNewContact() throws Exception {
+    }
+
+    @Test
+    public void updateContactDetails() throws Exception {
+    }
+
+    @Test
+    public void addPhoneNumber() throws Exception {
+    }
+
+    @Test
+    public void updatePhoneNumber() throws Exception {
+    }
+
+}
